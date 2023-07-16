614bb382c71dc9d1cb6657fa163c864bcc21d6cc
rebecaCaroba <rebecacaroba@gmail.com>
2023-07-16 15:13:30 -0300

    Desenvolvendo conversor de moedas usando API

---
+++ Desafios/conversorViaAPI/conv.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversor de moeda</title>
+</head>
+
+<body>
+    <?php
+
+    //Cotação via API   
+    date_default_timezone_set("America/Sao_Paulo");
+    $inicio = date("m-d-Y", strtotime("-7 days"));
+    $final = date("m-d-Y");
+
+    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $final . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+
+    //Funçao de tratar dados em JSON
+    $dados = json_decode(file_get_contents($url), true);
+
+    $cotacao = $dados['value'][0]['cotacaoCompra'];
+
+    $real = floatval($_REQUEST["valor"]) ?? 0;
+
+    //Equivalencia em dolar
+    $dolar = $real / $cotacao;
+
+    //Mostrar resultado
+
+    // Formataçao de moedas internacional
+    //Bublioteca intl (Internalization PHP)
+    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+
+    echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong> <br><br>";
+
+
+
+    // echo "Seus R\$".number_format($real, 2, "," , ".")." equivalem a US\$".number_format($dolar, 2, ",",".")."<br><br>";
+    ?>
+</body>
+
+</html>

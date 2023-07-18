0037b61d7f8d120da824772edb6d4a2fb6c1b3b1
rebecaCaroba <rebecacaroba@gmail.com>
2023-07-18 13:43:20 -0300

    Estilizando o formulário

--- Desafios/conversorViaAPI/conv.php
+++ Desafios/conversorViaAPI/conv.php
@@ -2,43 +2,40 @@
 <html lang="pt-br">
 
 <head>
+    <link rel="stylesheet" href="./estilo.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conversor de moeda</title>
 </head>
 
 <body>
-    <?php
+    <section>
+        <?php
+        //Cotação via API
+        date_default_timezone_set("America/Sao_Paulo");
+        $inicio = date("m-d-Y", strtotime("-7 days"));
+        $final = date("m-d-Y");
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $final . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
 
-    //Cotação via API   
-    date_default_timezone_set("America/Sao_Paulo");
-    $inicio = date("m-d-Y", strtotime("-7 days"));
-    $final = date("m-d-Y");
+        //Funçao de tratar dados em JSON
+        $dados = json_decode(file_get_contents($url), true);
+        $cotacao = $dados['value'][0]['cotacaoCompra'];
+        $real = floatval($_POST["valor"]) ?? 0;
 
-    $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarPeriodo(dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@dataInicial=\'' . $inicio . '\'&@dataFinalCotacao=\'' . $final . '\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,dataHoraCotacao';
+        //Equivalencia em dolar
+        $dolar = $real / $cotacao;
 
-    //Funçao de tratar dados em JSON
-    $dados = json_decode(file_get_contents($url), true);
+        //Mostrar resultado
+        // Formataçao de moedas internacional
 
-    $cotacao = $dados['value'][0]['cotacaoCompra'];
+        //Bublioteca intl (Internalization PHP)
+        $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+        echo "<span>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong> <br><br></span>";
 
-    $real = floatval($_REQUEST["valor"]) ?? 0;
-
-    //Equivalencia em dolar
-    $dolar = $real / $cotacao;
-
-    //Mostrar resultado
-
-    // Formataçao de moedas internacional
-    //Bublioteca intl (Internalization PHP)
-    $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-
-    echo "Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong> <br><br>";
-
-
-
-    // echo "Seus R\$".number_format($real, 2, "," , ".")." equivalem a US\$".number_format($dolar, 2, ",",".")."<br><br>";
-    ?>
+        // echo "Seus R$".number_format($real, 2, "," , ".")." equivalem a US$".number_format($dolar, 2, ",",".")."<br><br>";
+        ?>
+        <a href="javascript:history.go(-1)" class="botao">Voltar a página anterior</a>
+    </section>
 </body>
 
 </html>

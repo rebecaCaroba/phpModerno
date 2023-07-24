106106489b7e426f051a23f50429c8f4ea87f965
rebecaCaroba <rebecacaroba@gmail.com>
2023-07-24 14:47:19 -0300

    Corrgindo erros de execução do código JavaScript

--- Desafios/ReajustadorDePrecos/index.php
+++ Desafios/ReajustadorDePrecos/index.php
@@ -8,39 +8,46 @@
 </head>
 <body>
     <?php 
-    $num = $_POST['num'] ?? 0;
-    $perc = $_POST['perc'] ?? 0;
+    $num = $_POST['num'] ?? 1;
+    $perc = $_POST['perc'] ?? 50;
     ?>
     <main>
         <h1>Informe os valores:</h1>
-        <form action="<?=$_SERVER['PHP_SELF']?>" 
-        method="post">
-        <div class="container">
-            <label for="num">Preço do produto(R$)</label>
-            <input type="number" name="num" id="idnum">
-        </div>
-        <div class="conatiner">
-            <label for="perc">Qual será o percentual do reajuste <b class="valor_b"></b>?</label><br>
-            <input type="range" id="perc" name="perc" min="0" max="100" onchange="pegarDados()">
-        </div>
-        <input type="submit" class="botao" value="Reajustar">
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="post">
+            <div class="container">
+                <label for="num">Preço do produto(R$)</label>
+                <input type="number" name="num" id="idnum" value="<?=$num?>">
+            </div>
+            <div class="container">
+                <label for="perc">Qual será o percentual do reajuste(<b class="valor_b"></b>)?</label><br>
+                <input type="range" id="perc" name="perc" min="0" max="100">
+            </div>
+            <input type="submit" class="botao" value="Reajustar">
         </form>
     </main>
     <section>
         <h1>Resultado:</h1>
         <?php 
-            // if(empty($num)){
-            //     echo "Por favor, preencha os dados corretamente";
-            //     return;
-            // }
-
-            $res = ($num * ($perc/100))+$num;
-            
-            $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
-
-            echo "O produto que cutava".numfmt_format_currency($padrao, $num, "BRL")." com <b>".$perc."% de aumento</b> vai passar a custar <b>".numfmt_format_currency($padrao,$res, "BRL")."</b> a partir de agora!";
+            if ($num <= 0) {
+                if (empty($num) || !empty($num)) {
+                    echo "O valor do produto deve ser maior e diferente de 0.";
+                }
+            } else {
+                $res = ($num * ($perc/100)) + $num;
+                $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
+                echo "O produto que custava ".numfmt_format_currency($padrao, $num, "BRL")." com <b>".$perc."% de aumento</b> vai passar a custar <b>".numfmt_format_currency($padrao,$res, "BRL")."</b> a partir de agora!";
+            }
         ?>
     </section>
-    <script src="script.js"></script>
+    <script>
+        const range = document.querySelector('#perc');
+        const container = document.querySelector('.valor_b');
+
+        container.textContent = `${range.value}%`;
+
+        range.addEventListener('input', function() {
+            container.textContent = `${this.value}%`;
+        });
+    </script>
 </body>
 </html>
